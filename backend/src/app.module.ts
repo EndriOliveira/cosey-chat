418356a7054c8ac6e54c1e2483bf7c156907dac0
EndriOliveira <endriojesus76@gmail.com>
2023-10-02 15:05:11 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
-import { ChatGateway } from './modules/chat/chat.gateway';
 import { ChatModule } from './modules/chat/chat.module';
 import { UserService } from './modules/user/user.service';
-import { UserController } from './modules/user/user.controller';
 import { UserModule } from './modules/user/user.module';
 import { RefreshTokenModule } from './modules/refreshToken/refreshToken.module';
-import { MessageService } from './modules/message/message.service';
 import { FileModule } from './modules/file/file.module';
 import { MessageModule } from './modules/message/message.module';
-import { MessageController } from './modules/message/message.controller';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { WinstonInterceptor } from './interceptors/winston.interceptor';
 import { WinstonModule } from 'nest-winston';
@@ -29,11 +25,9 @@ import { PassportModule } from '@nestjs/passport';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     WinstonModule.forRoot(winstonConfig),
   ],
-  controllers: [AppController, UserController, MessageController],
+  controllers: [AppController],
   providers: [
-    ChatGateway,
     UserService,
-    MessageService,
     JwtStrategy,
     { provide: APP_INTERCEPTOR, useClass: WinstonInterceptor },
   ],
